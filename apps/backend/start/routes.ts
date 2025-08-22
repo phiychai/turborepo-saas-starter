f257d50f9de56c11e5214a6d7ee1529d62580d03
phiychai <3500928+phiychai@users.noreply.github.com>
2025-08-22 22:35:53 +0100

    feat: add adonis-autoswagger

--- apps/backend/start/routes.ts
+++ apps/backend/start/routes.ts
@@ -8,7 +8,19 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import AutoSwagger from 'adonis-autoswagger'
+import swagger from '#config/swagger'
 
+// returns swagger in YAML
+router.get('/swagger', async () => {
+  return AutoSwagger.default.docs(router.toJSON(), swagger)
+})
+// Renders Swagger-UI and passes YAML-output of /swagger
+router.get('/docs', async () => {
+  return AutoSwagger.default.ui('/swagger', swagger)
+  // return AutoSwagger.default.scalar("/swagger"); to use Scalar instead. If you want, you can pass proxy url as second argument here.
+  // return AutoSwagger.default.rapidoc("/swagger", "view"); to use RapiDoc instead (pass "view" default, or "read" to change the render-style)
+})
 router.get('/', async () => {
   return {
     hello: 'world',
